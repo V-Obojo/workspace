492e82760e2347f0cdf913e20e7d7624dc238948
V-Obojo <victorobojo@gmail.com>
2024-11-29 04:30:26 +0100

    day1

--- 30_days_bash_scripting/day01/bash_scripts/task1.sh
+++ 30_days_bash_scripting/day01/bash_scripts/task1.sh
@@ -1,28 +1,7 @@
 #!/bin/bash
 
-:'
-## Basic Bash Commands
-
-- `echo`: Print text to the console
-- `ls`: List directory contents
-- `cd`: Change directory
-- `mkdir`: Create a new directory
-- `touch`: Create an empty file
-- `chmod`: Change file permissions
-
-## Exercise
-
-Create a Bash script that:
-
-1. Creates a new directory called "my_project"
-2. Changes into that directory
-3. Creates three empty files named "file1.txt", "file2.txt", and "file3.txt"
-4. Lists the contents of the directory
-5. Prints a message saying "Project setup complete!"
-'
-
 mkdir my_project
 cd my_project
 touch file1.txt file2.txt file3.txt
 ls
-echo "Project setup complete!
+echo "Project setup complete"
